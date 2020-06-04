caaee9baf769d412e11c7870af3fe1f4e08e3aea
RockeR26 <65093060+RockeR26@users.noreply.github.com>
2020-06-04 18:15:26 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -13,21 +13,25 @@ $("#ru").click(function(){
 });
 $('#start').click(function(){
     if(!start){
+        nextSeq();
         start=true;
-        setTimeout(function(){
-            nextSeq();
-            $(".btn").click(function(){
-                var userColor=$(this).attr("id");
-                music(userColor);
-                anime(userColor);
-                userClickedPattern.push(userColor);
-                check();
-            });
-        },300);
+        //setTimeout(function(){
+            
     }
 });
+$(".btn").click(function(){
+    var userColor=$(this).attr("id");
+    music(userColor);
+    anime(userColor);
+    userClickedPattern.push(userColor);
+    check(userClickedPattern.length-1);
+});     
+        //},300);
+    
+
+
 function nextSeq(){
-    userClickedPattern=[];
+    userClickedPattern = [];
     level++;
     randomNo=Math.floor(Math.random()*4);
     randomColor=colors[randomNo];
@@ -36,33 +40,39 @@ function nextSeq(){
     gamePattern.push(randomColor);
     $("h2").text("Level "+level);
 }
+
 function music(val){
     var audio=new Audio("sounds/"+val+".mp3");
     audio.play();
 }
+
 function anime(color){
     $("#"+color).addClass("pressed")
     setTimeout(function(){
         $("#"+color).removeClass("pressed");  
     },100)  
 }
+
 function startOver(){
     start=false;
     level=0;
-    gamePattern=[];
+    gamePattern = [];
 }
-function check(){
-    if(userClickedPattern[(userClickedPattern.length-1)]===gamePattern[(userClickedPattern.length-1)]){
+
+function check(i){
+    if(userClickedPattern[i]===gamePattern[i]){
         if(userClickedPattern.length===gamePattern.length){
             setTimeout(function(){
                 nextSeq();
             },1000);        
-    }}
+        } 
+    }
     else{
-        $("body").addClass("game-over")
-        setTimeout(function(){ $("body").removeClass("game-over")},400);
         music("wrong");
+        $("body").addClass("game-over");
         $("h2").text("Game Over ☹! Press Start Button to restart");
+        setTimeout(function(){ $("body").removeClass("game-over")},400);
+      
         startOver();
     }
 }
